d486864356a3feffd4cd2053273c51ce0418977b
= <=>
2021-11-28 15:00:07 +0530

    Configured psql for Heroku

--- unchained/settings.py
+++ unchained/settings.py
@@ -77,9 +77,24 @@ WSGI_APPLICATION = 'unchained.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd6t60vsqqoso3q',
+
+        'USER': 'dbmonntrzjnrrr',
+
+        'PASSWORD': '8ba96f80f937ec298ac937ef46f60ef98f1515643112a053c76b4d5d83002e17',
+
+        'HOST': 'ec2-34-250-16-127.eu-west-1.compute.amazonaws.com',
+
+        'PORT': '5432',
+
     }
 }
 
